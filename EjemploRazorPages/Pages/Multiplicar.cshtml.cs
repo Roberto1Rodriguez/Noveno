2dce3e00ece066f667cae407c8dc0e46aa92768a
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2023-08-28 17:59:19 -0600

    TablaMultiplicarListo

--- EjemploRazorPages/Pages/Multiplicar.cshtml.cs
+++ EjemploRazorPages/Pages/Multiplicar.cshtml.cs
@@ -5,8 +5,20 @@ namespace EjemploRazorPages.Pages
 {
     public class MultiplicarModel : PageModel
     {
+        [BindProperty]
+        public int Numero1 { get; set; }
+        [BindProperty]
+        public int Numero2 { get; set; }
+        [BindProperty]
+        public int? Resultado { get; set; }
+
         public void OnGet()
         {
+
         }
+        public void OnPost() 
+        {
+            Resultado = Numero1 * Numero2;
+        }  
     }
 }
