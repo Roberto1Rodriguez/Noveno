f23a23dd50c87b70b0ee5b9a1a697e0a258b5473
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2023-08-28 17:41:02 -0600

    Segunda Page

--- EjemploRazorPages/Pages/Index.cshtml.cs
+++ EjemploRazorPages/Pages/Index.cshtml.cs
@@ -1,13 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography.X509Certificates;
 
 namespace EjemploRazorPages.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<string> Tabla { get; set; }=new List<string>();
         public void OnGet(int tabla)
         {
-
+            for (int i = 0; i <= 10; i++)
+            {
+                string o = $"{tabla}x{i}={tabla*i}";
+                Tabla.Add(o);
+            }
         }
     }
 }
