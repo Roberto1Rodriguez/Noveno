ec63bf7f37fe44a5a34212a2d50bcb957463315b
Roberto1Rodriguez <robertordzw@gmail.com>
2023-09-27 18:36:26 -0600

    Panes

--- EjemploRazorPages/wwwroot/scripts/ejercicio1.js
+++ EjemploRazorPages/wwwroot/scripts/ejercicio1.js
@@ -3,14 +3,30 @@
 const url ="https://integracion.itesrc.net/api/panes"
 
 var listaProductos;
+const botonarea = document.querySelector(".panes div");
 async function descargarProductos()
 {
     let response = await fetch(url);
 
     if (response.ok) {
         listaProductos = await response.json();
+        crearBotones();
                
     }
+
+}
+function crearBotones()
+{
+    listaProductos.forEach(x => {
+        let btn = document.createElement("BUTTON");
+        btn.dataset.id = x.id;
+        let img = document.createElement("IMG");
+        img.src = x.imagen;
+        let txt = document.createTextNode("$" + x.precio.toFixed(2));
+        btn.append(img, txt);
+        botonarea.append(btn);
+
+    });
 
 }
 descargarProductos();
