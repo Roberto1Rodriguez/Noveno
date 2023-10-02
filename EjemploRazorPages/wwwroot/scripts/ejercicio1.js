ba3343d40c2a9a1b25f62243e06787c80cf6a14b
Roberto1Rodriguez <robertordzw@gmail.com>
2023-10-02 17:23:06 -0600

    Final

--- EjemploRazorPages/wwwroot/scripts/ejercicio1.js
+++ EjemploRazorPages/wwwroot/scripts/ejercicio1.js
@@ -2,8 +2,10 @@
 
 const url ="https://integracion.itesrc.net/api/panes"
 
-var listaProductos;
+var listaProductos,saldo=0;
 const botonarea = document.querySelector(".panes div");
+const tabla = document.querySelector(".tabla tbody");
+const boton = document.querySelector("input[type='button']");
 async function descargarProductos()
 {
     let response = await fetch(url);
@@ -29,4 +31,38 @@ function crearBotones()
     });
 
 }
+
+botonarea.addEventListener("click", function (e) {
+    if (e.target.tagName == "BUTTON" || e.target.tagName == "IMG") {
+        let btn = e.target.tagName == "BUTTON" ? e.target : e.target.parentElement;
+        let id = btn.dataset.id;
+
+        let art = listaProductos.find(x => x.id == id);
+        if (art) {
+/*            let row = tabla.rows.find(x => x.dataset.id == id);*/
+            let row = tabla.querySelector(`[data-id="${id}"]`);
+            if (row) {// lo encontro
+
+                let cantidad = parseInt(row.cells[1].textContent) + 1;
+                row.cells[1].textContent = cantidad;
+                let subtotal = cantidad * art.precio;
+                row.cells[3].textContent = "$" + subtotal.toFixed(2);
+
+            }
+            else {// no lo encontro
+                row = tabla.insertRow();
+                row.dataset.id = id;
+                row.insertCell().textContent = art.descripcion;
+                row.insertCell().textContent = 1;
+                row.insertCell().textContent = "$" + art.precio.toFixed(2);
+                row.insertCell().textContent = "$" + art.precio.toFixed(2);
+            
+
+            }
+            saldo += art.precio;
+
+            boton.value = "Cobrar: $" + saldo.toFixed(2);
+        }
+    }
+} );
 descargarProductos();
