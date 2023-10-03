284c845c0883651c13a06c84c034fe77edbe3a51
Roberto1Rodriguez <robertordzw@gmail.com>
2023-10-02 18:00:12 -0600

    encuestav1

---
+++ EjemploRazorPages/Pages/ApiUse/Encuesta/Resultados.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EjemploRazorPages.Pages.ApiUse.Encuesta
+{
+    public class ResultadosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
